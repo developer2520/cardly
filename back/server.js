1a7856b9c5c70afd421fb9410a3ca667eec70ba6
developer2520 <berkino2099@gmail.com>
2025-01-02 13:08:07 +0500

    Ready to host backend

--- back/server.js
+++ back/server.js
@@ -142,5 +142,6 @@ app.get("/cards/:url", async (req, res) => {
 
 
 // Start Server
-const PORT = 4000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
