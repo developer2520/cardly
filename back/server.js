fa27c10e16a292d70da43762988ea54d45f8eae1
developer2520 <berkino2099@gmail.com>
2025-01-04 23:27:36 +0500

    dsf4tr

--- back/server.js
+++ back/server.js
@@ -20,16 +20,11 @@ app.use(express.json());
 
 // CORS Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Accept'],
-  exposedHeaders: ['Set-Cookie']
+  origin: ['http://localhost:5173'] ing cookies and credentials
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Remove 'Cookie' (not required here)
+  exposedHeaders: ['Set-Cookie'], // Expose 'Set-Cookie' to the client
 }));
-
 // Session Middleware
 app.use(
   session({
