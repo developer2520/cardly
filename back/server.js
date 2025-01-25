35f7e3bc8eca0516ff3618cd8de94bcf51abf8ce
developer2520 <berkino2099@gmail.com>
2025-01-25 18:28:53 +0500

    samesite issue fix i hope

--- back/server.js
+++ back/server.js
@@ -41,7 +41,7 @@ app.use(
     }),
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: process.env.NODE_ENV === 'production', // Enable secure cookies in production
       httpOnly: true,
     },
