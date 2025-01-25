b59bb64fe10d178e47717876ba7ad030550fef13
developer2520 <berkino2099@gmail.com>
2025-01-25 16:43:36 +0500

    forgot to change passport.js

--- back/passport.js
+++ back/passport.js
@@ -29,7 +29,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:4000/auth/google/callback', // Ensure this matches the URL in Google Cloud Console
+      callbackURL: 'https://cardly-seven.vercel.app/auth/google/callback',
+ // Ensure this matches the URL in Google Cloud Console
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
